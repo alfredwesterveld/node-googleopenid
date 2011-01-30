78a9b323d47adbd6b2e1577a6bd221d80f5963d3
Alfred Westerveld <alfredwesterveld@gmail.com>
2011-01-30 03:35:13 +0100

    updated require to use from npm and squashed a tiny bug with response

--- examples/index.js
+++ examples/index.js
@@ -1,18 +1,18 @@
-require.paths.unshift('../lib');
+//require.paths.unshift('../lib');
 
 const PORT = 8888;
 const HOST = 'localhost';
 
 const express = require('express');
-const openid = require('openid');
+//const openid = require('openid');
 const app = module.exports = express.createServer();
 const CALLBACK = 'http://localhost:8888/googleopenid/verify';
 const onAuthentication = function(req, res, openid_result) {
     console.log(openid_result);
-    res.send(openid_result.identifier);
+    res.send("" + openid_result.identifier);
 }
 
-const GoogleOpenid = require('GoogleOpenid').create(app, CALLBACK, onAuthentication);
+const GoogleOpenid = require('google-openid').create(app, CALLBACK, onAuthentication);
 
 app.use(
     express.staticProvider(__dirname + '/public')
