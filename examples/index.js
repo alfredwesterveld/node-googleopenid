1b0a0495653b088917174391191b892698366d85
Alfred Westerveld <alfredwesterveld@gmail.com>
2011-01-31 01:21:49 +0100

    baseURL is better description then hostname

--- examples/index.js
+++ examples/index.js
@@ -6,26 +6,31 @@ var HOST = 'localhost';
 var express = require('express');
 var app = module.exports = express.createServer();
 
+/**
+ * Called when Google's OpenID authentication has completed.
+ * You need to modify this to your needs.
+ */
 var onAuthentication = function(req, res, openid_result) {
     console.log(openid_result);
     res.send("" + openid_result.identifier);
 }
 
-require('google-openid').create(app, 'http://localhost:8888', onAuthentication);
+var baseURL = 'http://localhost:8888'; // base URL to map to your host
+require('google-openid').create(app, baseURL, onAuthentication);
 
 /**
- * Route middleware to showsa link to redirect to Google's OpenID provider.
+ * Route Middleware to show a link to redirect to Google's OpenID provider.
+ * You probably should replace this with a better view.
  */
 function googlelogin(req, res, next) {
     res.send('<html><head><title>Google Openid</title></head><body>' +
         '<a href="/googleopenid/authenticate">Sign in with your ' +
         '<img src="http://www.google.com/favicon.ico" border="0" />' +
         ' account</a></body></html>');
-        
 }
 
 /**
- * this route uses googlelogin route middleware.
+ * this route uses googlelogin Route Middleware.
  */
 app.get('/', googlelogin, function(req, res) {
 });
